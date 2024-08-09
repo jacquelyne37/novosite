73c0bf553eba091e9ff43065925f673adb108b50
jacquelyne37 <jacquelynelinda27@gmail.com>
2024-08-08 20:24:43 -0400

    atualizacao08/08/2024

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.http.response import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as login_django
 
-# Create your views here.
+def login(request):
+    # tentando fazer longin 
+    if request.method=='GET':
+        return render(request, 'usuarios/login.html')
+        # fazer formulário
+    else: 
+       username = request.POST.get('email')
+       senha =  request.POST.get('senha')
+     # modo de usuario verificar se esta correto!
+
+       user = authenticate(username= username, password=senha)
+
+       # autenticação se existe o usuario
+       if user:
+        login_django(request, user)
+        return HttpResponse('Autenticado!')
+        
+       else:
+            return HttpResponse('E-mail ou senha inválidos!')
+        
